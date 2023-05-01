c704a55a728e7db043a4afc82e9fa625348db6a2
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-01 20:49:27 +0600

    new added

---
+++ Basic-typeScript/src/alis.ts
@@ -0,0 +1,19 @@
+type CrushType = {
+    name : string;
+    age?: number;
+    profession : string;
+    address : string
+}
+
+const crush1 : CrushType= {
+    name : "Shamima Alina",
+    age : 23,
+    profession : "web Developer",
+    address : "Dhaka"
+}
+
+const crush2 : CrushType = {
+    name : "My tiya pakhi",
+    profession : "Next level web develper",
+    address : "London"
+}
