edba1caaaf0238eb800f32190d00b1d3af3035ec
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-02 11:56:57 +0600

    question mark

--- Basic-typeScript/src/question-mark.ts
+++ Basic-typeScript/src/question-mark.ts
@@ -1,6 +1,6 @@
 // ternary operator
 
-const age : number = 23;
+// const age : number = 23;
 
 // if(age >= 18){
 //     console.log('Yes');
@@ -9,11 +9,36 @@ const age : number = 23;
 // }
 
 
-const isAdult = age >= 18 ? "Yes" : "No"
-console.log(isAdult);
+// const isAdult = age >= 18 ? "Yes" : "No"
+// console.log(isAdult);
 
 // nullish coalesnace operator
 // null and undefined
 
 const isAuthenticatedUser = null 
 const userName = isAuthenticatedUser ?? "Guset"
+const userName2 = isAuthenticatedUser ? isAuthenticatedUser : "Guest"
+console.log({userName}, {userName2});
+
+
+type Manush = {
+    name : string,
+    age : number,
+    address : {
+        city : 'No City',
+        road : 'No road',
+        home?: ""
+    }
+}
+const manush1 : Manush={
+    name : "Human",
+    age : 200,
+    address : {
+        city : "No City",
+        road : "No road",
+        
+    }
+}
+
+const home = manush1?.address?.home?? 'No home' // default "no home"
+console.log({home});
