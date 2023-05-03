262ce17084411219e3b9e6396a03f13f408f3a05
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-03 11:49:11 +0600

    generic interface

--- Basic-advanceTypeScript/src/alis.ts
+++ Basic-advanceTypeScript/src/alis.ts
@@ -1,39 +1,39 @@
-type CrushType = {
-	name: string;
-	age?: number;
-	profession: string;
-	address: string;
-};
+// type CrushType = {
+// 	name: string;
+// 	age?: number;
+// 	profession: string;
+// 	address: string;
+// };
 
-const crush1: CrushType = {
-	name: "Shamima Alina",
-	age: 23,
-	profession: "web Developer",
-	address: "Dhaka",
-};
+// const crush1: CrushType = {
+// 	name: "Shamima Alina",
+// 	age: 23,
+// 	profession: "web Developer",
+// 	address: "Dhaka",
+// };
 
-const crush2: CrushType = {
-	name: "My tiya pakhi",
-	profession: "Next level web develper",
-	address: "London",
-};
+// const crush2: CrushType = {
+// 	name: "My tiya pakhi",
+// 	profession: "Next level web develper",
+// 	address: "London",
+// };
 
-type CrushMarriedType = boolean;
-const isCrushMarried: CrushMarriedType = false;
+// type CrushMarriedType = boolean;
+// const isCrushMarried: CrushMarriedType = false;
 
-type CourseNameType = string;
-const CourseName: CourseNameType = "Next level web development";
+// type CourseNameType = string;
+// const CourseName: CourseNameType = "Next level web development";
 
-type OperationType = (x: number, y: number) => number;
-const calculate = (
-	num1: number, // 10
-	num2: number, // 20
-	operation: OperationType
-	// (x, y) => x + y) (10,20) => 30
-) => {
-	return operation(num1, num2);
-};
+// type OperationType = (x: number, y: number) => number;
+// const calculate = (
+// 	num1: number, // 10
+// 	num2: number, // 20
+// 	operation: OperationType
+// 	// (x, y) => x + y) (10,20) => 30
+// ) => {
+// 	return operation(num1, num2);
+// };
 
-calculate(10, 20, (x, y) => x + y);
-calculate(10, 20, (x, y) => x - y);
-calculate(10, 20, (x, y) => x * y);
+// calculate(10, 20, (x, y) => x + y);
+// calculate(10, 20, (x, y) => x - y);
+// calculate(10, 20, (x, y) => x * y);
