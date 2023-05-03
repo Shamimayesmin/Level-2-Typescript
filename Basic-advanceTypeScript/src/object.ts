15ec681367470ed08885751d5e05baba80791f43
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-03 09:58:03 +0600

    interface type

--- Basic-advanceTypeScript/src/object.ts
+++ Basic-advanceTypeScript/src/object.ts
@@ -1,11 +1,11 @@
-const user : {
-    name : string,
-    age : number,
-    isMarried : boolean,
-    wife? : string
-} = {
-    name : "Monntu mia",
-    age : 57,
-    isMarried : true,
+// const user : {
+//     name : string,
+//     age : number,
+//     isMarried : boolean,
+//     wife? : string
+// } = {
+//     name : "Monntu mia",
+//     age : 57,
+//     isMarried : true,
     
-}
+// }
