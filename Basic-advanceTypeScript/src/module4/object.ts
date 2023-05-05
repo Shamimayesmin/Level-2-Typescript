7020b2172ab26fd7b25a1e9897f4907f1ec126dd
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-05 16:51:31 +0600

    type guard

--- Basic-advanceTypeScript/src/module4/object.ts
+++ Basic-advanceTypeScript/src/module4/object.ts
@@ -1,18 +1,18 @@
-class Animal {
-	// Parameter properties
-	constructor(
-		public name: string,
-		public species: string,
-		public sound: string
-	) {}
+// class Animal {
+// 	// Parameter properties
+// 	constructor(
+// 		public name: string,
+// 		public species: string,
+// 		public sound: string
+// 	) {}
 
-	// method
-	public makeSound() {
-		console.log(`The ${this.name} says ${this.sound}`); //
-	}
-}
+// 	// method
+// 	public makeSound() {
+// 		console.log(`The ${this.name} says ${this.sound}`); //
+// 	}
+// }
 
-const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
-const cat = new Animal("Persian", "cat", "meaw meaw");
-dog.makeSound();
-cat.makeSound();
+// const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+// const cat = new Animal("Persian", "cat", "meaw meaw");
+// dog.makeSound();
+// cat.makeSound();
