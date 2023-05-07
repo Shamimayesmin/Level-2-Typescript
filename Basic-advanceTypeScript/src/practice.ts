51019074a289b7799edfda2a4147a6dd20c88e4c
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-07 16:45:08 +0600

    new added

---
+++ Basic-advanceTypeScript/src/practice.ts
@@ -0,0 +1,8 @@
+// practice -1
+
+const userInfo = [101, "Ema", "John", true,  , "2023"];
+
+const userInfo1 :[number, string,string,boolean,undefined, string] =[101, "Ema", "John", true,  , "2023"]
+
+// practice -2
+
