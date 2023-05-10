20fa24d04aa55345fe5388c0ef68d4d678c8b415
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-10 21:09:24 +0600

    advance ts

--- Basic-advanceTypeScript/src/module3/interface.ts
+++ Basic-advanceTypeScript/src/module3/interface.ts
@@ -1,56 +1,56 @@
-type User = {
-    name : string,
-    age : number
-};
+// type User = {
+//     name : string,
+//     age : number
+// };
 
-type extendUser = User & {
-    role : string,
-}
+// type extendUser = User & {
+//     role : string,
+// }
 
-interface IUser {
-    name : string,
-    age : number
-}
+// interface IUser {
+//     name : string,
+//     age : number
+// }
 
-interface IExtendUser extends IUser{
-    role : string
-}
+// interface IExtendUser extends IUser{
+//     role : string
+// }
 
-const user : extendUser = {
-    name : 'Omanush',
-    age : 2000,
-    role : 'Unknown'
-}
-const userWithTypeAlias : User = {
-    name : "Type Alias",
-    age : 100,
-}
-
-
-
-const userWithInteface : IUser = {
-    name : "Interface",
-    age : 200,
-}
-
-type rollNumber = number;
-type addNumbersType = (num1 : number, num2 : number) => number;
-
-interface IAddNumbers {
-    (num1 : number, num2 : number): number;
-}
-
-type rollNumberType = number[];
-
-interface IRollNumbers {
-    [index : number] : string;
-
-}
-
-// const rollNumbers : IRollNumbers = ["1","3","5"]  // [index]
+// const user : extendUser = {
+//     name : 'Omanush',
+//     age : 2000,
+//     role : 'Unknown'
+// }
+// const userWithTypeAlias : User = {
+//     name : "Type Alias",
+//     age : 100,
+// }
 
 
-const addNumbers : addNumbersType= (num1, num2): number => num1 + num2;
+
+// const userWithInteface : IUser = {
+//     name : "Interface",
+//     age : 200,
+// }
+
+// type rollNumber = number;
+// type addNumbersType = (num1 : number, num2 : number) => number;
+
+// interface IAddNumbers {
+//     (num1 : number, num2 : number): number;
+// }
+
+// type rollNumberType = number[];
+
+// interface IRollNumbers {
+//     [index : number] : string;
+
+// }
+
+// // const rollNumbers : IRollNumbers = ["1","3","5"]  // [index]
 
 
-// const addNumbers = (num1 : number, num2 : number): number => num1 + num2;
+// const addNumbers : addNumbersType= (num1, num2): number => num1 + num2;
+
+
+// // const addNumbers = (num1 : number, num2 : number): number => num1 + num2;
